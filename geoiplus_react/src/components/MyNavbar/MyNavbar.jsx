b451ab34b3a1c910d8601698cb3df4c97eaafdfe
JuliaShubina <JuliaShakir@gmail.com>
2024-12-17 08:40:50 +0200

    add navbar

---
+++ geoiplus_react/src/components/MyNavbar/MyNavbar.jsx
@@ -0,0 +1,29 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./MyNavbar.css";
+// import { Link } from "react-router-dom";
+
+const MyNavbar = () => {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="md" className="full-height-navbar">
+        <Container>
+          <Navbar.Brand className="logo" href="#home">
+            Geoinnovation+
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#About">About</Nav.Link>
+              <Nav.Link href="#Experience">Experience</Nav.Link>
+              <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#Contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default MyNavbar;
