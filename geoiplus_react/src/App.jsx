5c6b45e05f579a3e55a76eaafffc2eea93d95f80
JuliaShubina <JuliaShakir@gmail.com>
2024-12-17 08:40:21 +0200

    migration Geoiplus on React

---
+++ geoiplus_react/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/base.css";
+
+import Header from "./components/header/Header";
+import { Container } from "react-bootstrap";
+import ContainerFluidBreakpointExample from "./components/TestNavbar";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
