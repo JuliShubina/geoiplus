4382fa0b39739aa47f7f408644dc27ada749e10d
JuliaShubina <JuliaShakir@gmail.com>
2025-01-03 16:50:38 +0200

    refactoring

--- geoiplus_react/src/components/portfolio section/Portfolio.jsx
+++ geoiplus_react/src/components/portfolio section/Portfolio.jsx
@@ -1,10 +1,11 @@
 import { Container } from "react-bootstrap";
 import "./portfolio.css";
-import road_damages_preview from "../../img/portfolio/preview/road_damages_preview.png";
-import asbestos_project_preview from "../../img/portfolio/preview/asbestos_project_preview.png";
-import smart_survey_preview from "../../img/portfolio/preview/smart_survey_preview.png";
-import vegetation_management_preview from "../../img/portfolio/preview/vegetation_management_preview.png";
-import oblique_viewer_preview from "../../img/portfolio/preview/oblique_viewer_preview.png";
+import road_damages_preview from "../../data/img/portfolio/preview/road_damages_preview.png";
+import asbestos_project_preview from "../../data/img/portfolio/preview/asbestos_project_preview.png";
+import smart_survey_preview from "../../data/img/portfolio/preview/smart_survey_preview.png";
+import vegetation_management_preview from "../../data/img/portfolio/preview/vegetation_management_preview.png";
+import oblique_viewer_preview from "../../data/img/portfolio/preview/oblique_viewer_preview.png";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
@@ -23,9 +24,9 @@ const Portfolio = () => {
                 <h4>Object detection for road damages</h4>
                 <p>ML/AI</p>
 
-                <a href="portfolio-road-damages.html" className="details-link" title="More Details">
+                <Link to={"portfolio-road-damages"} className="details-link" title="More Details">
                   <i className="bx bx-link"></i>
-                </a>
+                </Link>
               </div>
             </div>
 
@@ -35,9 +36,9 @@ const Portfolio = () => {
                 <h4>Asbestos project</h4>
                 <p>SW development, data processing</p>
 
-                <a href="portfolio-asbestos-project.html" className="details-link" title="More Details">
+                <Link to={"portfolio-asbestos-project"} className="details-link" title="More Details">
                   <i className="bx bx-link"></i>
-                </a>
+                </Link>
               </div>
             </div>
 
@@ -47,9 +48,9 @@ const Portfolio = () => {
                 <h4>Smart survey</h4>
                 <p>SW development</p>
 
-                <a href="portfolio-smart-survey.html" className="details-link" title="More Details">
+                <Link to={"portfolio-smart-survey"} className="details-link" title="More Details">
                   <i className="bx bx-link"></i>
-                </a>
+                </Link>
               </div>
             </div>
 
@@ -59,9 +60,9 @@ const Portfolio = () => {
                 <h4>Vegetation management</h4>
                 <p>3D analysis</p>
 
-                <a href="portfolio-vegetation-management.html" className="details-link" title="More Details">
+                <Link to={"portfolio-vegetation-management"} className="details-link" title="More Details">
                   <i className="bx bx-link"></i>
-                </a>
+                </Link>
               </div>
             </div>
 
@@ -71,9 +72,9 @@ const Portfolio = () => {
                 <h4>Oblique viewer plugin</h4>
                 <p>SW development</p>
 
-                <a href="portfolio-oblique-viewer.html" className="details-link" title="More Details">
+                <Link to={"portfolio-oblique-viewer"} className="details-link" title="More Details">
                   <i className="bx bx-link"></i>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
