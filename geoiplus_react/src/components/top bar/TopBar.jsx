a65486468dd257c148b9ded21e5ee4714701bc5d
JuliaShubina <JuliaShakir@gmail.com>
2025-01-03 16:47:34 +0200

    add topBar

---
+++ geoiplus_react/src/components/top bar/TopBar.jsx
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap";
+import "./TopBar.css";
+
+const TopBar = () => {
+  return (
+    <section id="topbar" className="d-flex align-items-center">
+      <Container>
+        <div className="contact-info ">
+          <i className="bi bi-envelope-fill"></i>
+          <a href="mailto:contact@geoiplus.com">info@geoiplus.com</a>
+          <i className="bi bi-phone-fill phone-icon"></i>
+          +48 500 718 764
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default TopBar;
