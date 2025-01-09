71f1a5e1e481529d5ca4b67c917e960ab8395fcf
JuliaShubina <JuliaShakir@gmail.com>
2025-01-09 14:41:04 +0200

    added header fixation

--- geoiplus_react/src/components/header/Header.jsx
+++ geoiplus_react/src/components/header/Header.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
 import MyNavbar from "../MyNavbar/MyNavbar";
-import NavBarForCP from "../navBar for CardPortffolio/navBarForCP";
+import NavBarForCP from "../navBar for CardPortfolio/navBarForCP";
 import "./header.css";
 import PropTypes from "prop-types";
 
-const Header = ({ from }) => {
+const Header = ({ from, place }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.getElementById("header");
+      if (header) {
+        const headerOffset = header.offsetTop;
+        // console.log(headerOffset);
+        const scrolled = window.scrollY;
+        // console.log(scrolled);
+        if (scrolled > headerOffset) {
+          header.classList.add("fixed-top");
+          // const headerChild = document.querySelector("header").firstElementChild;
+          setIsScrolled(true);
+        } else {
+          header.classList.remove("fixed-top");
+          setIsScrolled(false);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header id="header">
-      {from === "Main" && <MyNavbar />}
-      {from === "CardPortfolio" && <NavBarForCP />}
+      {from === "Main" && <MyNavbar isScrolled={isScrolled} />}
+      {from === "CardPortfolio" && <NavBarForCP place={place} />}
     </header>
   );
 };
