ad28652a42466b42b86e8e6c44e85fc7b031e639
JuliaShubina <JuliaShakir@gmail.com>
2025-01-09 14:31:14 +0200

    add 404 page

---
+++ geoiplus_react/src/components/not found page/NotFoundPage.jsx
@@ -0,0 +1,21 @@
+import { Container } from "react-bootstrap";
+import Header from "../header/Header";
+
+const NotFoundPage = () => {
+  let place = "NotFoundPage";
+  return (
+    <>
+      <Header from="CardPortfolio" place={place} />
+      <main className="d-flex align-items-center">
+        <Container>
+          <div className="section-title ">
+            <span>404 Not Found</span>
+            <h2>404 Not Found</h2>
+          </div>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default NotFoundPage;
