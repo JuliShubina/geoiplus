9d89f911874c404fe49961a42c6fde615d414139
JuliaShubina <JuliaShakir@gmail.com>
2025-01-10 13:33:21 +0200

    fixed file name

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import Footer from "../footer/Footer.jsx";
-import BackToTop from "../back to-top/BackToTop.jsx";
+import BackToTop from "../backToTop/BackToTop.jsx";
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
